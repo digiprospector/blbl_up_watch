d44f4babfbd6789671488da8f0b3089048c55273
digiprospector <digiprospector@protonmail.com>
2025-08-22 14:39:24 +0800

    update

--- dp_bilibili_api.py
+++ dp_bilibili_api.py
@@ -9,6 +9,8 @@ import json
 from functools import reduce
 import urllib.parse
 import hashlib
+from pathlib import Path
+from pprint import pprint
 
 class dp_bilibili:
     def __init__(self, ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3", cookies=None, logger=None, retry_max=10, retry_interval=5):
@@ -16,7 +18,6 @@ class dp_bilibili:
         self.session = requests.Session()
         self.session.headers.update({'User-Agent': self.ua})
         if cookies:
-            self.set_cookies(cookies)
             self.session.cookies.update(cookies)
         if logger:
             self.logger = self.logger
@@ -33,8 +34,11 @@ class dp_bilibili:
         self.retry_max = retry_max
         self.retry_interval = retry_interval
         self.get_wbi_keys()
+        self.mid = 0
+        self.name = ""
 
-    def login_by_qrcode(self):
+
+    def login_by_qrcode(self) -> bool:
         """通过二维码扫描进行登录并返回一个包含cookies的session对象"""
         # 1. 获取二维码URL和key
         login_url_api = "https://passport.bilibili.com/x/passport-login/web/qrcode/generate"
@@ -61,7 +65,7 @@ class dp_bilibili:
         
         try:
             while True:
-                time.sleep(1)  # 等待一段时间后再轮询
+                time.sleep(3)  # 等待一段时间后再轮询
                 params = {'qrcode_key': qrcode_key}
                 poll_response = self.session.get(poll_api, params=params) # 此处将自动使用 session 的 headers
                 poll_response.raise_for_status()
@@ -80,7 +84,7 @@ class dp_bilibili:
             self.logger.info(f"轮询登录状态时发生错误: {e}")
             return False
         
-    def login(self):
+    def login(self) -> bool:
         if not self.test_login():
             self.logger.info("请重新扫码登录")
             self.login_by_qrcode()
@@ -91,7 +95,7 @@ class dp_bilibili:
                 self.logger.info("登录成功")
                 return True
         else:
-            self.logger.info("已经登录，无需重复登录")
+            self.logger.info("已经登录，无需扫码登录")
             return True
 
     def test_login(self):
@@ -101,7 +105,9 @@ class dp_bilibili:
             response.raise_for_status()
             data = response.json().get('data', {})
             if data.get('isLogin'):
-                self.logger.info(f"已经登录 {data.get('uname')} mid:{data.get('mid')}")
+                self.mid = data.get('mid', 0)
+                self.name = data.get('uname', "")
+                self.logger.info(f"已经登录 {self.name} mid:{self.mid}")
                 return True
             else:
                 return False
@@ -118,7 +124,7 @@ class dp_bilibili:
             data = response.json()
             if data['code'] == 0:
                 # 包含默认的“全部关注”和“悄悄关注”等
-                self.groups = {group['name']: group['tagid'] for group in data['data']}
+                self.groups = {group['tagid']: {'name':group['name'], 'count':group['count']} for group in data['data']}
             else:
                 self.logger.info(f"获取关注分组失败: {data['message']}")
                 self.groups = {}
@@ -185,15 +191,15 @@ class dp_bilibili:
         params['w_rid'] = w_rid
         return params
 
-    def get_up_videos(self, mid):
+    def get_up_videos(self, mid, ps=30, pn=1):
         """获取UP主第一页视频信息"""
 
         
         # 构造基本参数
         params = {
             "mid": mid,
-            "ps": 30,       # 每页视频数
-            "pn": 1,        # 页码
+            "ps": ps,       # 每页视频数
+            "pn": pn,        # 页码
             "order": "pubdate",
             "platform": "web",
             "web_location": "1550101"
@@ -224,30 +230,198 @@ class dp_bilibili:
                 return []
             
             # 提取视频数据
-            videos = []
+            videos = {}
             for video in data["data"]["list"]["vlist"]:
                 title = video["title"]
                 bvid = video["bvid"]
-                link = f"https://www.bilibili.com/video/{bvid}"
-                videos.append({"title": title, "link": link, "bvid": bvid})
+                videos[bvid] = {'title':title}
             
             return videos
         
         except Exception as e:
             self.logger.info(f"请求发生错误: {e}")
-            return []
+            return {}
+
+    def get_ups_in_group(self, tag_id: int, pn: int = 1, ps: int = 300):
+        """根据分组ID获取关注的UP主列表，失败时会自动重试。"""
+        api_url = "https://api.bilibili.com/x/relation/tag"
+        params = {
+            "mid": self.mid,
+            "tagid": tag_id,
+            "pn": 1,
+            "ps": 300,  # 每页数量，对于此API，通常一次返回分组内所有UP主
+        }
+        headers = {
+            "Referer": f"https://space.bilibili.com/{self.mid}/fans/follow",
+        }
+        self.session.headers.update(headers)
+
+        for attempt in range(self.retry_max):
+            try:
+                # session中已包含User-Agent
+                response = self.session.get(api_url, params=params, timeout=10)
+                response.raise_for_status()
+                data = response.json()
+                if data.get('code') == 0:
+                    # 成功获取，返回数据
+                    ups = {data["mid"]: {'name':data["uname"]} for data in data.get("data", {})}
+                    return ups
+                else:
+                    # API返回错误码，打印信息并重试
+                    self.logger.info(f"获取分组关注列表失败 (尝试 {attempt + 1}/{self.retry_max}): {data.get('message')}")
+            except Exception as e:
+                # 请求或解析过程发生异常，打印信息并重试
+                self.logger.info(f"请求关注列表时发生错误 (尝试 {attempt + 1}/{self.retry_max}): {e}")
+                
+            if attempt < self.retry_max - 1:
+                self.logger.info(f"将在 {self.retry_interval} 秒后重试...")
+                time.sleep(self.retry_interval)
+            else:
+                self.logger.info("已达到最大重试次数，获取关注列表失败。")
+                
+        return {} # 所有重试都失败后
+    
+    def get_video_info(self, bvid):
+        """获取视频信息"""
+
+        api_url = "https://api.bilibili.com/x/web-interface/view"
+        params = {
+            "bvid": bvid
+        }
+        
+        headers = {
+            "Referer": f"https://www.bilibili.com/video",
+        }
+
+        for attempt in range(self.retry_max):
+            try:
+                # session中已包含User-Agent
+                response = self.session.get(api_url, params=params, headers=headers, timeout=10)
+                response.raise_for_status()
+                data = response.json()
+                if data.get('code') == 0:
+                    # 成功获取，返回数据
+                    data_json = data.get("data", {})
+                    video_info = {data_json["bvid"]:{'pubdate':data_json["pubdate"],'title':data_json['title'],'duration':data_json['duration'], 'cid':data_json['cid']}}
+                    return video_info
+                else:
+                    # API返回错误码，打印信息并重试
+                    self.logger.info(f"获取视频信息失败 (尝试 {attempt + 1}/{self.retry_max}): {data.get('message')}")
+            except Exception as e:
+                # 请求或解析过程发生异常，打印信息并重试
+                self.logger.info(f"请求视频信息时发生错误 (尝试 {attempt + 1}/{self.retry_max}): {e}")
+                
+            if attempt < self.retry_max - 1:
+                self.logger.info(f"将在 {self.retry_interval} 秒后重试...")
+                time.sleep(self.retry_interval)
+            else:
+                self.logger.info("已达到最大重试次数，获取视频信息失败。")
+                
+        return {} # 所有重试都失败后
+    
+    def get_audio_download_url(self, bvid, cid):
+        """获取视频下载链接"""
+
+        api_url = "https://api.bilibili.com/x/player/wbi/playurl"
+        params = {
+            'fnval': 16,  # 16表示dash格式的视频
+            "bvid": bvid,
+            "cid": cid
+        }
+        
+        for attempt in range(self.retry_max):
+            try:
+                # session中已包含User-Agent
+                response = self.session.get(api_url, params=params, timeout=10)
+                response.raise_for_status()
+                data = response.json()
+                if data.get('code') == 0:
+                    # 成功获取，返回数据
+                    data_json = data.get("data", {})
+                    audio_json_list = data_json.get("dash", {}).get("audio", [])
+                    # 优先选择id为30280, 30232, 30216的音频
+                    target_ids = [30280, 30232, 30216]
+                    selected_audio = ""
+                    for target_id in target_ids:
+                        for audio in audio_json_list:
+                            if audio.get("id") == target_id:
+                                selected_audio = audio.get('baseUrl', "")
+                                break
+                    return selected_audio
+                else:
+                    # API返回错误码，打印信息并重试
+                    self.logger.info(f"获取视频下载链接失败 (尝试 {attempt + 1}/{self.retry_max}): {data.get('message')}")
+            except Exception as e:
+                # 请求或解析过程发生异常，打印信息并重试
+                self.logger.info(f"请求视频下载链接时发生错误 (尝试 {attempt + 1}/{self.retry_max}): {e}")
+                
+            if attempt < self.retry_max - 1:
+                self.logger.info(f"将在 {self.retry_interval} 秒后重试...")
+                time.sleep(self.retry_interval)
+            else:
+                self.logger.info("已达到最大重试次数，获取视频下载链接失败。")
+                
+        return {} # 所有重试都失败后
+
+def download_file_with_resume(session, url, file_path:Path):
+    headers = {}
+    # 检查是否已存在部分下载的文件
+    if file_path.exists():
+        file_size = file_path.stat().st_size
+        headers['Range'] = f'bytes={file_size}-'
+    
+    try:
+        response = session.get(url, headers=headers, stream=True, timeout=30)
+        
+        # 检查服务器是否支持断点续传
+        if response.status_code == 206:  # 部分内容
+            mode = 'ab'  # 追加模式
+        elif response.status_code == 200:  # 全部内容
+            mode = 'wb'  # 写入模式
+        else:
+            print(f"服务器返回异常状态码: {response.status_code}")
+            return False
+        
+        with open(file_path, mode) as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file.write(chunk)
+        
+        print("下载完成!")
+        return True
+        
+    except Exception as e:
+        print(f"下载过程中出现错误: {e}")
+        return False
 
 if __name__ == "__main__":
-    dp_blbl = dp_bilibili()
-    dp_blbl.login()
+    cookies = {}
+    cookies_file = Path("cookies.json")
+    if cookies_file.exists():
+        with open("cookies.json", "r") as f:
+            cookies = json.load(f)
+    dp_blbl = dp_bilibili(cookies=cookies)
+    if dp_blbl.login():
+        with open("cookies.json", "w") as f:
+            json.dump(dp_blbl.session.cookies.get_dict(), f)
     gp = dp_blbl.get_following_groups()
-    dp_blbl.logger.info(f"关注分组: {gp}")
-    
-    if gp:
-        group_name = next(iter(gp))
-        group_id = gp[group_name]
-        dp_blbl.logger.info(f"第一个分组: {group_name}, ID: {group_id}")
-        dp_blbl.get_followings_in_group(group_id)
-    else:
-        dp_blbl.logger.info("没有关注分组")
-    
+    dp_blbl.logger.debug(f"关注分组: {gp}")
+    group_id, (group_name, ups_count) = next(iter(gp.items()))  # 获取第一个分组名称
+    dp_blbl.logger.info(f"第一个分组: {group_name}, ID: {group_id}, UP主数量: {ups_count}")
+    ups =dp_blbl.get_ups_in_group(group_id)
+    dp_blbl.logger.info(f"分组 {group_name} 中的UP主: {ups}")
+    up_id, up_name = next(iter(ups.items()))  # 获取第一个UP主
+    dp_blbl.logger.info(f"第一个UP主: {up_name}, ID: {up_id}")
+    videos = dp_blbl.get_up_videos(up_id)
+    dp_blbl.logger.info(f"UP主 {up_name} 的视频列表: {videos}")
+    bvid, title = next(iter(videos.items()))  # 获取第一个视频
+    dp_blbl.logger.info(f"第一个视频: {title}, BV号: {bvid}")
+    video_info = dp_blbl.get_video_info(bvid)
+    dp_blbl.logger.info(f"视频 {title} 的详细信息: {video_info}")
+    with open("video_info.json", "w") as f:
+        json.dump(video_info, f, ensure_ascii=False, indent=4)
+    dl_url = dp_blbl.get_audio_download_url(bvid, video_info[bvid]['cid'])
+    dp_blbl.logger.info(f"视频 {title} 的下载链接: {dl_url}")
+    with open("download_url.json", "w") as f:
+        json.dump(dl_url, f, ensure_ascii=False, indent=4)
+    download_file_with_resume(dp_blbl.session, dl_url, Path(f"audio.m4s"))
